09c0d15785aba4384f6daedd91a681f2f769eeb0
Michelle <153286968+MischkaPCHI@users.noreply.github.com>
2024-03-18 10:01:49 +0100

    Update img.js

--- src/img.js
+++ src/img.js
@@ -1,27 +1,20 @@
-import heightCalculator from "./images/O6g8qQCmsP4.jpg";
-import flexAndGrid from "./images/photo_2024-02-23_21-48-18.jpg";
-import bit from "./images/uo1f4QxAZII.jpg";
-import bag from "./images/YoGbaWdZhvY (1).jpg";
-import programer from "./images/Снимок экрана 2024-02-08 121347.png";
-import htmlMem from "./images/Снимок экрана 2024-02-08 122019.png";
-
 export const arrayOfImg = [
-    {
-        memImg: heightCalculator
-    },
-    {
-        memImg: flexAndGrid
-    },
-    {
-        memImg: bit
-    },
-    {
-        memImg: bag
-    },
-    {
-        memImg: programer
-    },
-    {
-        memImg: htmlMem
-    }
+  {
+    memImg: "/images/O6g8qQCmsP4.jpg",
+  },
+  {
+    memImg: "/images/photo_2024-02-23_21-48-18.jpg",
+  },
+  {
+    memImg: "/images/uo1f4QxAZII.jpg",
+  },
+  {
+    memImg: "/images/YoGbaWdZhvY (1).jpg",
+  },
+  {
+    memImg: "/images/Снимок экрана 2024-02-08 121347.png",
+  },
+  {
+    memImg: "/images/Снимок экрана 2024-02-08 122019.png",
+  },
 ];
