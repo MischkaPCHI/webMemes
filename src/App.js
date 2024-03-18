7caa997b0013d80f557612665888101c1eb5eb4f
MischkaPCHI <michellejung4002@gmail.com>
2024-03-18 10:02:36 +0100

    img

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import './style.css';
-import Button from './components/Button';
-import {arrayOfImg} from './img';
-import {links} from './link';
-
-const Img = (props) => {
-  return(
-    <img src={props.src} alt="" />
-  );
-};
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import "./style.css";
+import Button from "./components/Button";
+import { arrayOfImg } from "./img";
+import { links } from "./link";
+// import facebook from './facebook.svg';
 
 function App() {
   const [now, setNow] = useState(new Date());
 
   setInterval(() => setNow(new Date()), 1000);
+
   return (
     <div>
-    <div className="header">
+      <div className="header">
         <div className="logo-wrapper">
-            <img className="logo" src={logo} alt="" />
+          <img className="logo" src={logo} alt="" />
         </div>
 
-      {
-        links.map((link, i) => <Button key={i}>{link.title}</Button>)
-      }
-
-    </div>
-
-    <div className='time'>Time: {now.toLocaleTimeString()}</div>
-    <h1>Memes</h1>
-
-    <div className="img">
-        {/* <img src={arrayOfImg[0]} alt="" /> */}
-        {/* <img src="./images/Снимок экрана 2024-02-08 122019.png" alt="" /> */}
-        {arrayOfImg.map((img, i) => (
-          <Img key={i} src={img.memImg} />
+        {links.map((link, i) => (
+          <Button key={i} url={link.link}>
+            {link.title}
+          </Button>
         ))}
+      </div>
+
+      <div className="time">Time: {now.toLocaleTimeString()}</div>
+      <h1>Memes</h1>
+
+      <div className="img">
+        {arrayOfImg.map((img, i) => (
+          <img key={i} src={img.memImg} alt="" />
+        ))}
+      </div>
+
+      <footer>{/* <img src={facebook}></img> */}</footer>
     </div>
-</div>
   );
 }
 
